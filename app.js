932fe8577646c1595592764423ed8b36c4491fb0
Trevor Snyder <trevsny@gmail.com>
2018-11-19 15:24:02 -0600

    Ticking clock assignment.  Both in Vanilla js and using React

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const myButton = React.createElement('button', null, "Click Me");
-console.log(JSON.stringify(myButton));
+// const myButton = React.createElement('button', null, "Click Me");
+ // console.log(JSON.stringify(myButton));
+
+const vanillaDiv = document.getElementById("vanilla");
+// const date = new Date().toLocaleTimeString()
+
+const renderNow = () => {
+    vanillaDiv.innerHTML = `
+    <div class="demo">
+        <p>Vanilla - The time is: ${new Date().toLocaleTimeString()}</p>
+    </div>`;
+
+    ReactDOM.render(
+        
+        React.createElement('p', null, `React - the time is: ${new Date().toLocaleTimeString()}`),
+        document.getElementById('app'),
+        );
+}
+
+setInterval(renderNow, 1000);
 
 
-ReactDOM.render(
-    myButton, document.getElementById('app')
-);
+
